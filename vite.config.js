35f4531a0f3fa06c860872e25f16c722493709dc
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2024-05-14 16:53:59 +0200

    config files + basic html

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+
+const exercisePath = "C:\\Users\\user3\\OneDrive\\Pulpit\\Projekt zaliczeniowy\\Projekt-zaliczeniowy\\index.html";
+
+
+export default defineConfig({
+    root: exercisePath,
+    server: {
+        port: 3000,
+    },
+    plugins: [react()],
+});
