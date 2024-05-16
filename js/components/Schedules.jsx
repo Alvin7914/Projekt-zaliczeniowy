741f6cb49386740c149ef80b72ddf9c4dfe94a91
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2024-05-16 23:48:23 +0200

    styling and partly done jsx (table) for schedules, small alterations in header (fixed link display)

--- js/components/Schedules.jsx
+++ js/components/Schedules.jsx
@@ -2,7 +2,67 @@ import React from "react";
 
 const Schedules = () => {
     return (
-        <h2>PLAN ZAJĘĆ</h2>
+        <div className='schedules__box'>
+            <table className='schedules__table'>
+                <caption>
+                    Tygodniowy plan zajęć
+                </caption>
+                <thead>
+                <tr>
+                    <th>Godziny</th>
+                    <th>Poniedziałek</th>
+                    <th>Wtorek</th>
+                    <th>Środa</th>
+                    <th>Czwartek</th>
+                    <th>Piątek</th>
+                    <th>Sobota</th>
+                    <th>Niedziela</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>16.00 - 17.00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>17.00 - 18.00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>18.00 - 19.00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>19.00 - 20.00</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     );
 };
 
