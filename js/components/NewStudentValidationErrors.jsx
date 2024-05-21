e3e7da0c39fea413d94d29e692d1ba5858d2c138
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2024-05-21 18:37:48 +0200

    cosmetic changes in errors styles

--- js/components/NewStudentValidationErrors.jsx
+++ js/components/NewStudentValidationErrors.jsx
@@ -7,7 +7,7 @@ const NewStudentValidationErrors = ({errorsArray}) => {
             {errorsArray.map((error, index) => <p // mapowanie tablicy z errorami, otrzymanej w props
                 className='error-message'
                 key={index}
-                style={{color: "red"}}>{error}</p>)}
+                style={{color: "red", maxWidth: '200px', marginBottom: '10px'}}>{error}</p>)}
         </div>
     );
 };
