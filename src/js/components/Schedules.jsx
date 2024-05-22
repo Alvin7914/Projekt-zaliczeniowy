bf2747b58bbb1320beccc828b6d4ff02a0f7c479
Alvin7914 <krzysztof.kamieniarz791@wp.pl>
2024-05-22 23:39:09 +0200

    restoring schedules progress

--- src/js/components/Schedules.jsx
+++ src/js/components/Schedules.jsx
@@ -1,6 +1,69 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const Schedules = () => {
+    const [classesTable, setClassesTable] = useState([]);
+    const [studentsList, setStudentsList] = useState([]);
+
+    useEffect(() => {
+        const savedClasses = JSON.parse(localStorage.getItem('classesList')) || [];
+        setClassesTable(savedClasses);
+
+        const savedStudents = JSON.parse(localStorage.getItem('studentsList')) || [];
+        setStudentsList(savedStudents);
+    }, []);
+
+    const switchHidden = (e) => {
+        e.preventDefault();
+
+        const button = document.querySelector('.new-entry-btn');
+        const form = document.querySelector('.schedules__form');
+
+        button.classList.toggle('hidden');
+        form.classList.toggle('hidden');
+    };
+
+    const renderTableCell = (day, time) => {
+        const classForCell = classesTable.find(classEntry => classEntry.day === day && classEntry.time === time);
+        return classForCell ? `${classForCell.student.name}, ${classForCell.student.address}` : '';
+    };
+
+    const handleCellClick = (day, time) => {
+        setClassesTable(state => {
+            const updatedClasses = state.filter(classEntry => !(classEntry.day === day && classEntry.time === time));
+            localStorage.setItem('classesList', JSON.stringify(updatedClasses));
+            return updatedClasses;
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const selectStudentId = document.querySelector('#students').value;
+        const selectDay = document.querySelector('#day').value;
+        const selectTime = document.querySelector('#time').value;
+
+        const selectedStudent = studentsList.find(student => student.id === parseInt(selectStudentId));
+
+        if (!selectedStudent) {
+            alert('Nie znaleziono ucznia');
+            return;
+        }
+
+        switchHidden(e);
+
+        const newClass = {
+            id: (localStorage.getItem('classesList') === null || JSON.parse(localStorage.getItem('classesList')).length === 0)
+                ? 1
+                : JSON.parse(localStorage.getItem('classesList'))[JSON.parse(localStorage.getItem('classesList')).length - 1].id + 1,
+            student: selectedStudent,
+            day: selectDay,
+            time: selectTime,
+        }
+
+
+        setClassesTable(state => [...state, newClass])
+        localStorage.setItem('classesList', JSON.stringify([...classesTable, newClass]))
+    };
+
     return (
         <section className='schedules__box'>
             <table className='schedules__table'>
@@ -20,48 +83,51 @@ const Schedules = () => {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>16.00 - 17.00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>17.00 - 18.00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>18.00 - 19.00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>19.00 - 20.00</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                {['16.00-17.00', '17.00-18.00', '18.00-19.00', '19.00-20.00'].map(time => (
+                    <tr key={time}>
+                        <td>{time.replace('-', ' - ')}</td>
+                        {['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'].map(day => (
+                            <td key={day} onClick={() => handleCellClick(day, time)}>{renderTableCell(day, time)}</td>
+                        ))}
+                    </tr>
+                ))}
                 </tbody>
             </table>
+            <button className='new-entry-btn' onClick={switchHidden}>Dodaj zajęcia</button>
+            <form className='schedules__form hidden' onSubmit={handleSubmit}>
+                <div className='schedules__form-column'>
+                    <label htmlFor="students">Wybierz ucznia</label>
+                    <select name="students" id="students">
+                        {studentsList.map((student) => (
+                            <option key={student.id} value={student.id}>
+                                {student.name} {student.surname}, {student.address}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className='schedules__form-column'>
+                    <label htmlFor="day">Wybierz dzień</label>
+                    <select name="day" id="day">
+                        <option value="monday">Poniedziałek</option>
+                        <option value="tuesday">Wtorek</option>
+                        <option value="wednesday">Środa</option>
+                        <option value="thursday">Czwartek</option>
+                        <option value="friday">Piątek</option>
+                        <option value="saturday">Sobota</option>
+                        <option value="sunday">Niedziela</option>
+                    </select>
+                </div>
+                <div className='schedules__form-column'>
+                    <label htmlFor="time">Wybierz godzinę</label>
+                    <select name="time" id="time">
+                        <option value="16.00-17.00">16.00 - 17.00</option>
+                        <option value="17.00-18.00">17.00 - 18.00</option>
+                        <option value="18.00-19.00">18.00 - 19.00</option>
+                        <option value="19.00-20.00">19.00 - 20.00</option>
+                    </select>
+                </div>
+                <button type='submit'>Zapisz</button>
+            </form>
         </section>
     );
 };
